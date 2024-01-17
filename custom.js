19966481bbb152e344e12b8592d633fdede896af
Rimshad <128842166+Rimshad01@users.noreply.github.com>
2024-01-17 07:19:39 +0530

    Add files via upload

--- custom.js
+++ custom.js
@@ -27,4 +27,13 @@ window.onload = calcScrollValue;
 //     // loader.style.display = "none";
 // })
 
+var icon = document.getElementById("icon");
 
+icon.onclick = function(){
+    document.body.classList.toggle("light-theme");
+    if(document.body.classList.contains("light-theme")){
+        icon.src = "moon.png";
+    }else{
+        icon.src = "sun.png";
+    }
+}
